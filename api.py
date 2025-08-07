3fd381decdb37b0417d3e045bbecd558f16c85d2
wenjlo77 <wenjlo77@gmail.com>
2025-08-07 21:25:42 +0800

    update api

--- api.py
+++ api.py
@@ -1,10 +1,8 @@
 from flask import Flask
-import pandas as pd
 from flask_cors import CORS
 from module.source import ETToday
-from LLM.llm import LargeLanguageModel,gemini
-from langchain.prompts import PromptTemplate
-from LLM.prompt import prompt_news
+from LLM.llm import gemini
+
 from langchain.chains import LLMChain
 app = Flask(__name__)
 CORS(app)
@@ -13,5 +11,6 @@ def curl():
     news = ETToday()
     news.output(scroll_count=3,chain=gemini)
     return 'curl finished...'
+
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0")
