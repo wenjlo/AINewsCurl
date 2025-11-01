908f300464d6ad99ecc180c3e660594849e3005e
wenjlo77 <wenjlo77@gmail.com>
2025-11-01 18:33:11 +0800

    update source

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from module.source import ETToday
-from langchain.prompts import PromptTemplate
-from LLM.prompt import prompt_news
-from langchain.chains import LLMChain
 
-prompt_template = PromptTemplate.from_template(prompt_news)
 
 if __name__ == "__main__":
     news = ETToday()
